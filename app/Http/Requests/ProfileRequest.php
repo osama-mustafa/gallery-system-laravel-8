6c9fc0d50d9a5f2b734cb280a2beb2c467bfcf3d
osama-mustafa <56209750+osama-mustafa@users.noreply.github.com>
2021-06-13 06:57:04 +0200

    Create RequestForm for Profile

---
+++ app/Http/Requests/ProfileRequest.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Validation\Rule;
+
+class ProfileRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules()
+    {
+        return [
+            'name' => [
+                'required',
+                Rule::unique('users')->ignore(Auth::id())
+            ],
+
+            'email' => [
+                'required',
+                Rule::unique('users')->ignore(Auth::id())
+            ]
+        ];
+    }
+}
