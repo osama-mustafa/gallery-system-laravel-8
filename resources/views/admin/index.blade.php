7c2b79c840adf2dd9cd1fbff8ac1e42912907ea0
osama-mustafa <56209750+osama-mustafa@users.noreply.github.com>
2021-06-13 06:39:12 +0200

    fix formatting

--- resources/views/admin/index.blade.php
+++ resources/views/admin/index.blade.php
@@ -4,9 +4,7 @@
 @section('content')
     <div class="container">
         <div class="row mt-3">
-
             @if (auth()->user()->admin == true)    
-            
                 <div class="col-xl-3 col-md-6">
                     <div class="card bg-secondary text-white mb-4">
                         <div class="card-body"><i class="fas fa-users"></i> Users: {{ $users->count() }}</div>
@@ -56,10 +54,7 @@
                         <a href="{{ route('images.create') }}" class="btn btn-secondary">Upload now</a>
                     </h4>    
                 </div>
-
-
             @endif
-
         </div>
     </div>
 @endsection
